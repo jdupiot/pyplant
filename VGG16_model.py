ef5a1fcc061c2870b82cf83e2aa2bbec15563254
Jennifer <jdupiot@gmail.com>
2020-12-20 20:41:54 +0100

    ajout evaluation modele

--- VGG16_model.py
+++ VGG16_model.py
@@ -151,3 +151,99 @@ print(model.predict(img))
 print(model.predict(img).flatten().max())
 
 
+#########################
+# evaluation du modele
+#########################
+
+#courbes pour voir loss et acc en fonction des epochs
+train_acc = history.history['acc']
+val_acc = history.history['val_acc']
+
+train_loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+plt.figure(figsize=(10,10))
+plt.subplot(121)
+plt.xlabel("Epochs")
+plt.ylabel("loss")
+plt.title("Model loss by epoch")
+plt.plot(train_loss)
+plt.plot(val_loss)
+plt.legend(['train','test'])
+
+plt.subplot(122)
+plt.xlabel("Epochs")
+plt.ylabel("acc")
+plt.title("Model acc by epoch")
+plt.plot(train_acc)
+plt.plot(val_acc)
+plt.legend(['train','test'])
+plt.show();
+plt.savefig(outputdir"/VGG16_test_1_accuracy_curve.jpg",format='jpg')
+
+
+def convert_image(X):
+    X_img=[]
+    for image in X:
+        # Load image
+        img=cv2.imread(image)
+        # Resize image
+        img=cv2.resize(img,(224,224))
+        # for the black and white image
+        if img.shape==(224, 224):
+        	img = image.img_to_array(img)
+        	img = np.expand_dims(img, axis=0)
+        	img = img/255
+        	#img=img.reshape([224,224,1])
+            #img=np.concatenate([img,img,img],axis=2)  
+        # cv2 load the image BGR sequence color (not RGB)
+        X_img.append(img[...,::-1])
+    return np.array(X_img)
+
+X_train_img = convert_image(data_train.file)
+y_train = data_train['disease']
+
+X_test_img = convert_image(data_test.file)
+y_test = data_test['disease']
+
+#scores
+#X_test= ? et Y_test ?
+scores = model.evaluate(X_test_img,y_test)
+print("Fonction de perte : %s" % scores[0])
+print("Précision du modèle : %s" % scores[1])
+
+
+
+test_pred = model.predict(X_test_img)
+test_pred_class = test_pred.argmax(axis=1)
+y_test_class = y_test.argmax(axis=1)
+
+print(metrics.classification_report(y_test_class,test_pred_class))
+
+
+################
+# matrice de confusion
+
+cnf_matrix = metrics.confusion_matrix(y_test_class, test_pred_class)
+classes = range(0,3)
+
+plt.figure()
+plt.imshow(cnf_matrix, interpolation='nearest',cmap='Blues')
+plt.title("Matrice de confusion")
+plt.colorbar()
+tick_marks = np.arange(len(classes))
+plt.xticks(tick_marks, classes)
+plt.yticks(tick_marks, classes)
+
+for i, j in itertools.product(range(cnf_matrix.shape[0]), range(cnf_matrix.shape[1])):
+    plt.text(j, i, cnf_matrix[i, j],
+             horizontalalignment="center",
+             color="white" if cnf_matrix[i, j] > ( cnf_matrix.max() / 2) else "black")
+
+plt.ylabel('Vrais labels')
+plt.xlabel('Labels prédits')
+plt.show()
+
+plt.savefig("/media/PGM/bioinfo/Jennifer/DS/novembre/VGG16_test_1_matrice_confusion.jpg",format='jpg')
+################
+
